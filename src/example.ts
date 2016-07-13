3c256599ea53f065f7427360ed5aec478b737084
Noj Vek <nojvek@gmail.com>
2016-07-13 09:29:11 -0700

    Params should only be sent if they are not undefined

--- src/example.ts
+++ src/example.ts
@@ -23,13 +23,34 @@ import {EventEmitter} from 'events'
 // }
 // takeHeapTrace()
 
+interface WsRpcRequest {
+    id: number,
+    method: string,
+    params?: any
+}
+
+interface WsRpcResponse {
+    id: number,
+    method: string,
+    result?: any
+    error?: {
+        code: string,
+        message: string
+    }
+}
+
+interface WsRpcEvent {
+    method: string,
+    params?: any
+}
+
 class WsRpcClient extends EventEmitter {
     private static ConnectTimeout = 5000
 
     private _address: string
     private _connectedPromise: Promise<WsRpcClient>
     private _webSocket: WebSocket
-    private _pendingMessageMap: Map<number, any> = new Map()
+    private _pendingMessageMap: Map<number, {resolve: Function, reject: Function}> = new Map()
     private _nextMessageId: number = 0
 
     public static connect(address: string): Promise<WsRpcClient> {
@@ -64,10 +85,11 @@ class WsRpcClient extends EventEmitter {
 
             webSocket.on('message', messageStr => {
                 console.log("Response", messageStr)
-                const message = JSON.parse(messageStr)
+                const message: WsRpcResponse & WsRpcEvent = JSON.parse(messageStr)
 
                 if (message.id) {
-                    const id = parseInt(message.id)
+                    // TODO fix parseInt definition in typescript lib.d.ts
+                    const id = parseInt(<any>message.id)
                     if (this._pendingMessageMap.has(id)) {
                         // Resolve promise from message
                         const promise = this._pendingMessageMap.get(id)
@@ -78,8 +100,6 @@ class WsRpcClient extends EventEmitter {
                         } else {
                             console.error(`Invalid message: ${messageStr}`)
                         }
-                        console.log("Process promsise#", message.id, this._pendingMessageMap)
-                        this._pendingMessageMap.delete(message.id)
                     } else {
                         console.error(`Got a response with id ${message.id} for which there is no pending request.`)
                     }
@@ -91,20 +111,22 @@ class WsRpcClient extends EventEmitter {
     }
 
     public sendMessage(method: string, params?: any): Promise<any> {
-        const message = {
+        const message: WsRpcRequest = {
             id: ++this._nextMessageId,
-            method,
-            params
+            method
         }
 
+
+        if (params) message.params = params;
+
         const promise = new Promise((resolve, reject) => {
+            this._pendingMessageMap.set(message.id, {resolve, reject})
             this._connectedPromise.then(() => {
                 console.log("Request", message)
-                this._webSocket.send(JSON.stringify(message), reject)
+                this._webSocket.send(JSON.stringify(message))
             })
         })
 
-        this._pendingMessageMap.set(message.id, promise)
 
         return promise
     }
@@ -117,12 +139,12 @@ class WsRpcClient extends EventEmitter {
 
 async function main() {
     try {
-        //const client = await WsRpcClient.connect("ws://localhost:9222/devtools/page/c1375023-d638-4af7-b63a-7e4fcc774897")
-        const client = await WsRpcClient.connect("ws://localhost:9229/node")
+        const client = await WsRpcClient.connect("ws://localhost:9222/devtools/page/758107cd-dc4a-4263-b089-e2ef82260125")
+        //const client = await WsRpcClient.connect("ws://localhost:9229/node")
 
-        await client.sendMessage("Runtime.enable")
-        await client.sendMessage("Debugger.enable")
-        await client.sendMessage("Console.enable")
+        client.sendMessage("Runtime.enable")
+        client.sendMessage("Debugger.enable")
+        client.sendMessage("Console.enable")
         await client.sendMessage("Profiler.enable")
 
     } catch (e) {
